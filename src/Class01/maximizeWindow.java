b4f03ebb26bda2a958e91380e7dcbd1e0bfe8774
MayraMejia <mayramejia21018@gmail.com>
2022-10-27 15:39:25 -0400

    first commit

---
+++ src/Class01/maximizeWindow.java
@@ -0,0 +1,16 @@
+package Class01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class maximizeWindow {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "Driver/chromedriver");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.manage().window().fullscreen();
+        driver.quit();
+    }
+}
